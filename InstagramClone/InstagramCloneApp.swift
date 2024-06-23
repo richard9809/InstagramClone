d8387218150ddb8c21139061051747397546dd90
Richard Mulu <richard.ndisya@strathmore.edu>
2024-06-23 11:45:44 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Richard Mulu Ndisya on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
